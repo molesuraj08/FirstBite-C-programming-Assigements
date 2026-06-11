28eaee84f5275c6b078644b5ae90967169aeca30
molesuraj08 <molesuraj08@gmail.com>
2026-06-11 18:56:47 +0530

    Add files via upload

---
+++ Loops 3 Assigement/10_first_last_digit_sum.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int n, first, last;
+
+    scanf("%d", &n);
+
+    last = n % 10;
+
+    while(n >= 10)
+        n /= 10;
+
+    first = n;
+
+    printf("%d", first + last);
+
+    return 0;
+}
